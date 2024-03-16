9a8364df8bcbb8d3ec09f083211e3c29c668933a
Ratchanon Traitiprat <teenoigammer2546@gmail.com>
2024-03-16 23:09:33 +0200

    first commit

---
+++ src/main/java/dev/ratchanon/contentcalendar/ContentCalendarApplication.java
@@ -0,0 +1,15 @@
+package dev.ratchanon.contentcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentCalendarApplication.class, args);
+	}
+
+	//wrie more code
+
+}
